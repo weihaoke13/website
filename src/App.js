4a9cdb48a5ce7cf468e84dbde2dbe46060a86b17
weihaoke13 <weihaoke13@gmail.com>
2020-07-04 17:17:51 -0700

    fixed navbar+ in progress of project grid responsive

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends Component {
   render(){
     return(
       <div >
-        <Layout fixedHeader>
+        <Layout >
             <Header className='header-color' title={<Link style={{textDecoration:'none',color:'white'}} to="/">Weihao Ke | Portfolio</Link>}>
                 <Navigation>
-                    {/* <Link to="/landingpage">Home</Link> */}
+                    <Link to="/landingpage">Home</Link> 
                     {/* resume */}
                     <a href="https://resume.creddle.io/resume/84d5s0tgcn8" rel="noopener noreferrer " target="_blank">
                       Resume
                     </a>
-                    {/* <Link to="/aboutme">About Me</Link> */}
-                    {/* <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link> */}
+                     <Link to="/aboutme">About Me</Link> 
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link> 
                 </Navigation>
             </Header>
-            <Drawer title={<Link style={{textDecoration:'none',color:'grey'}} to="/">Weihao Ke | </Link>}>
+            <Drawer title={<Link style={{textDecoration:'none',color:'grey'}} to="/">Weihao Ke</Link>}>
                 <Navigation>
                     <Link to="/landingpage">Home</Link>
                     <a href="https://resume.creddle.io/resume/84d5s0tgcn8" rel="noopener noreferrer " target="_blank">
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -11,24 +11,24 @@ class LandingPage extends Component{
                         src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/71679939_2464225650331507_4169363528639053824_n.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_oc=AQmRBoaJWaKCxzepIbsT9xGkh_uTclykSo-PVUYjFtFYPKFhWTLOORS-fzJuqunx_mo&_nc_ht=scontent-lax3-1.xx&oh=9a3d1fe818c1380c42270bbb7f90dd47&oe=5F0922D5"
                         alt="avatar"
                         className="avatar-img">
-
                         </img>
 
                         <div className='banner-text'>
-                            <h1>Software Engineer</h1>
+                            <h1>Software Developer</h1>
                             <hr/>
-                            <p> HTML CSS | React.JS | Javascript </p>
+                            <p> HTML/CSS | React.JS | Javascript | Python | PostgreSQL </p>
+                           
                             <div className='social-links'>
                                 <a href="https://www.linkedin.com/in/weihao-ke-859702162" rel="noopener noreferrer " target="_blank">
-                                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
                                 </a>
                                 <a href="https://github.com/weihaoke13" rel="noopener noreferrer " target="_blank">
-                                    <i className="fa fa-github" aria-hidden="true"></i>
+                                    <i className="fa fa-github-square" aria-hidden="true"></i>
                                 </a>
                                 <a href="https://www.facebook.com/weihao.ke.5" rel="noopener noreferrer " target="_blank">
-                                    <i className="fa fa-facebook" aria-hidden="true"></i>
+                                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
                                 </a>
-                        </div>s
+                        </div>
                         </div>
                 
                     </Cell>
